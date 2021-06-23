7e34f6d7f0cd8338bdb8d2c347415ec5fc2cc6bc
Gashun Chagdonov <gowagowa1990@gmail.com>
2021-06-23 12:12:41 +0300

    добавляем Card через props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import Drawer from './components/Drawer';
 import Card from './components/Card';
 import Search from './components/Search';
 
+const arr = [
+  { title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12990, imgUrl: "/img/sneakers/1.jpg" },
+  { title: 'Мужские Кроссовки Nike Air Max 270', price: 8990, imgUrl: "/img/sneakers/2.jpg" },
+  { title: 'Кроссовки Puma X Aka Boku Future Rider', price: 9990, imgUrl: "/img/sneakers/3.jpg" },
+  { title: 'Мужские Кроссовки Under Armour Curry 8', price: 15990, imgUrl: "/img/sneakers/4.jpg" },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -11,11 +18,9 @@ function App() {
       <div className="content p-40">
         <Search />
         <div className="d-flex flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map(({title, price, imgUrl, index}) => (
+				 <Card key={`${index}_${title}`} title={title} price={price} img={imgUrl}/>
+			  ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({title, price, img}) => {
 	return (
 		<div className="card">
             <div className="favorite">
               <img src="/img/unliked.svg" alt="unliked" />
             </div>
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneakers" />
-            <h5 className="sl">Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={img} alt="sneakers" />
+            <h5 className="sl">{title}</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Цена: </span>
-                <b>12 999 руб.</b>
+                <b>{price}</b>
               </div>
-              <button className="button">
+              <button className="button" onClick={() => alert(123)}>
                 <img width={11} height={11} src="/img/plus.svg" alt="plus" />
               </button>
             </div>
